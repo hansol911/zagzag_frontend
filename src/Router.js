89420bb5c59116cfef63db3426f99603774bfb43
hansol911 <hansol@micehub.com>
2022-10-07 21:21:41 +0900

    router 기능 추가

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Main from "./Main";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
