347675eac30c024f2cae5a6fa2467833bc4b338b
hansol911 <hansol@micehub.com>
2022-10-21 21:46:02 +0900

    component 재사용 basic

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,27 +1,28 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "./Login.scss";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import UserInput from './UserInput/UserInput';
+import './Login.scss';
 
 const Login = () => {
   const navigate = useNavigate();
   const [auth, setAuth] = useState({
-    id: "",
-    pw: "",
+    id: '',
+    pw: '',
   });
 
-  const exactAuth = { id: "hansol", pw: "1234" };
+  const exactAuth = { id: 'hansol', pw: '1234' };
 
-  const changeAuth = (e) => {
+  const changeAuth = e => {
     const { name, value } = e.target;
-    setAuth((prev) => ({ ...prev, [name]: value }));
+    setAuth(prev => ({ ...prev, [name]: value }));
   };
 
   const clickLoginBtn = () => {
     if (auth.id === exactAuth.id && auth.pw === exactAuth.pw) {
-      alert("ok");
-      navigate("/main");
+      alert('ok');
+      navigate('/main');
     } else {
-      alert("failed!!");
+      alert('failed!!');
     }
   };
 
@@ -30,26 +31,20 @@ const Login = () => {
       <h1 className="title">ZAGZAG</h1>
       <div className="loginContainer">
         <div className="inputContainer">
-          <div className="idContainer">
-            <span className="itemName">Username</span>
-            <input
-              className="id"
-              type="text"
-              placeholder="아이디를 입력해주세요."
-              name="id"
-              onChange={changeAuth}
-            />
-          </div>
-          <div className="pwContainer">
-            <span className="itemName">Password</span>
-            <input
-              className="pw"
-              type="password"
-              placeholder="비밀번호를 입력해주세요."
-              name="pw"
-              onChange={changeAuth}
-            />
-          </div>
+          <UserInput
+            title="username"
+            name="id"
+            type="text"
+            placeholder="아이디를 입력해주세요."
+            changeAuth={changeAuth}
+          />
+          <UserInput
+            title="password"
+            name="pw"
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            changeAuth={changeAuth}
+          />
           <div className="forgetContainer">
             <span className="itemName">Forget password?</span>
           </div>
---
+++ src/pages/Login/UserInput/UserInput.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const UserInput = ({ title, name, type, placeholder, changeAuth }) => {
+  return (
+    <div className={`${name}Container`}>
+      <span className="itemName">{title}</span>
+      <input
+        className={name}
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        onChange={changeAuth}
+      />
+    </div>
+  );
+};
+
+export default UserInput;
